703236b6b9316a7025a0d2f0a316591c9b19dea9
johni08 <johnirevormasiziliwu@gmail.com>
2024-08-13 01:04:08 +0700

    authentication

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Validation\Rules;
+use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Database\Eloquent\Casts\Json;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Handle an incoming registration request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        $user['token'] = $request->user()->createToken('auth')->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'message' => 'sign up successfully',
+            'data' => $user,
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\Api\ListingController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return response()->json([
+        'success' => true,
+        'message' => "Detail logged in user",
+        'data' => $request->user(),
+    ]);
+});
+
+Route::resource('listing', ListingController::class)->only(['index', 'show']);
+
+require __DIR__.'/auth.php';
