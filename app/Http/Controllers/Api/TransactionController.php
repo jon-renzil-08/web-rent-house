893e2b25844a6ac63a761f3656c50ee04b2a9919
johni08 <johnirevormasiziliwu@gmail.com>
2024-08-13 01:11:14 +0700

    api transaction controller

---
+++ app/Http/Controllers/Api/TransactionController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Transaction\Store;
+use Illuminate\Http\Request;
+
+class TransactionController extends Controller
+{
+    private function __fullyBookedChecker(Store $request)
+    {
+        
+    }
+}
